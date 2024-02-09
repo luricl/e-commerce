70840c94e7ddd21545dcfdac7415c6a2b893d9a0
Lurian <luri.cl95@gmail.com>
2024-02-09 14:43:05 -0300

    cria o admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from auctions.models import User, Categories, AuctionListings, Bids, Comments
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Categories)
+admin.site.register(AuctionListings)
+admin.site.register(Bids)
+admin.site.register(Comments)
