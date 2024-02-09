2f2fa085b866fd8e16df3f54218c936fe58052a4
Lurian <luri.cl95@gmail.com>
2024-02-09 14:43:35 -0300

    atualizando modelos

--- auctions/models.py
+++ auctions/models.py
@@ -5,23 +5,28 @@ from django.db import models
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
 
+class Categories(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+
 class AuctionListings(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64, unique=True)
-    active = models.BooleanField(default=True)
-    description = models.CharField(max_length=150)
-    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
-    current_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name="auctions")
     image_url = models.URLField()
-    category = models.CharField(max_length=64)
-    users_watching = models.ManyToManyField(User)
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT, blank=True, related_name="auctions")
+    creation_date = models.DateTimeField(auto_now_add=True)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    users_watching = models.ManyToManyField(User, blank=True, related_name="watchlist")
 
     # https://www.geeksforgeeks.org/overriding-the-save-method-django-models/
     def save(self, *args, **kwargs):
         if not self.current_bid:
             self.current_bid = self.starting_bid
         super(AuctionListings, self).save(*args, **kwargs)
-
 
 class Bids(models.Model):
     id = models.AutoField(primary_key=True)
@@ -33,5 +38,6 @@ class Comments(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_author")
     auction_id = models.ForeignKey(AuctionListings, on_delete=models.CASCADE, related_name="commented_auction")
+    creation_date = models.DateTimeField(auto_now_add=True)
     body = models.CharField(max_length=10000)
     time = models.DateTimeField()
