46edad9ae2457bdd1a0e1508191d235d4ac1f4db
Lurian <luri.cl95@gmail.com>
2024-02-10 17:15:30 -0300

    refact create_auctions view

--- auctions/views.py
+++ auctions/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
 
-from .models import User, AuctionListings, Bids, Comments
+from .models import User, AuctionListings, Categories, Bids, Comments
 
 
 def index(request):
@@ -67,27 +67,40 @@ def register(request):
 def create_listing(request):
     if request.method == "POST":
         try:
+            author = request.user
+            category = Categories.objects.get(name=request.POST.get("category"))
 
             auction = AuctionListings(
                 title=request.POST.get("title"), 
+                author = author,
                 description=request.POST.get("description"),                     
                 starting_bid=float(request.POST.get("starting bid")), 
-                image_url=request.POST.get("image")
+                image_url=request.POST.get("image"),
+                category=category
             )
             
             auction.save()
-            
-            messages.success(request, "Listing created!")
-            
+                        
             return HttpResponseRedirect(reverse("index"))
         except:
             messages.error(request, "action failed!")
 
-    return render(request, "auctions/create_auction.html")
+    categories = Categories.objects.all()
+
+    return render(request, "auctions/create_auction.html",{
+                    "categories" : categories
+                })
     
 def show_auction(request, item):
 
     auction = AuctionListings.objects.get(title=item)
+    # watchlist = User.auctionlistings_set.filter(username=request.).all()
+
+    if request.method == "POST":
+        # post bids(if user is authenticaded), addition or removal from watchlist
+        # close auction if user is the author of the listing
+        # post comments if user is authenticated
+        pass
 
     return render(request, "auctions/auction.html", {
         "auction": auction 
